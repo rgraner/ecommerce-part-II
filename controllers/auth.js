cc71f7e2876b9b078fcc582b9db76688cbd82ddc
rgraner <ricardograner@gmail.com>
2023-05-12 08:54:29 +0100

    General update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,12 @@
-const passport = require('../loaders/passport');
+// const passport = require('../loaders/passport');
 
-module.exports = {
-  login: passport.authenticate('local', { failureRedirect: '/api/login-failure'}),
-  logout: (req, res) => {
-    req.logout();
-    res.json({ success:true });
-  },
-  loginFailure: (req, res) => {
-    res.status(401).json({ success: false, message: 'incorrect email or password.'});
-  }
-};
+// module.exports = {
+//   login: passport.authenticate('local', { failureRedirect: '/api/login-failure'}),
+//   logout: (req, res) => {
+//     req.logout();
+//     res.json({ success:true });
+//   },
+//   loginFailure: (req, res) => {
+//     res.status(401).json({ success: false, message: 'incorrect email or password.'});
+//   }
+// };
--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,4 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.status(200).json({ message: 'Login successful', user: req.user });
 });
 
-// router.post('/login', async (req, res, next) => {
-//   const { password } = req.body;
-//   const hashedPassword = await hashPassword(password);
-//   passport.authenticate('local', async (err, user, info) => {
-//     // remaining code
-//     res.status(200).json({ message: 'Login successful', user: req.user });
-//   })(req, res, next);
-// });
-
-
 module.exports = router;
